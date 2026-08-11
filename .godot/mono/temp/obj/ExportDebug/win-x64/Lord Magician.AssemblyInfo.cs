23a2472579e16d68371674239b49f22fd9343f2d
ChipironStudios-Die <chipironstudios@gmail.com>
2026-08-11 20:18:42 +0100

    arreglitos sin completar

--- .godot/mono/temp/obj/ExportDebug/win-x64/Lord Magician.AssemblyInfo.cs
+++ .godot/mono/temp/obj/ExportDebug/win-x64/Lord Magician.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lord Magician")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("ExportDebug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82844f7b6db69ae65c3b4c5eac2a89fb7959e550")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lord Magician")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lord Magician")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
