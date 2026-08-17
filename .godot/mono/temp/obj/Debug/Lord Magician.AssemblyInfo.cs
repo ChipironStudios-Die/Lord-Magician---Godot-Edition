a387c12fae48cb6fb5d8ee901ca87bef684c4ace
ChipironStudios-Die <chipironstudios@gmail.com>
2026-08-17 18:55:11 +0100

    cosinas

--- .godot/mono/temp/obj/Debug/Lord Magician.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Lord Magician.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lord Magician")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8867b9ba0ecbc13ee097fecf818d90009f3049ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a425e8687da2458b6f5e3a952a11e08d0644025")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lord Magician")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lord Magician")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
