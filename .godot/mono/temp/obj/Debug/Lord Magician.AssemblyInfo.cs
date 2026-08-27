0bfb58bc0e442fe1b2561860fa3c272b7c3e514a
ChipironStudios-Die <chipironstudios@gmail.com>
2026-08-27 15:12:56 +0100

    correción de caché

--- .godot/mono/temp/obj/Debug/Lord Magician.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Lord Magician.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lord Magician")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2825ae573ea6b0b3ef8e06c624dbf074ff0e347")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e5b6b755cdd4be2b8172fe7c910838269ab7774")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lord Magician")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lord Magician")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
